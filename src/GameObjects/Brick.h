cb3fbbb2818b1bdd68e862051215438a28ffa2a0
@artemsap <a.sapozhnikov00@gmail.com>
2025-09-27 14:08:18 +0300

    make brick generator

--- src/GameObjects/Brick.h
+++ src/GameObjects/Brick.h
@@ -3,13 +3,46 @@
 #include "StaticObject.h"
 #include "../Utils/Utils.h"
 
+#include <vector>
 #include <glm.hpp>
 
 class Brick : public StaticObject
 {
 public:
 	Brick(const glm::vec2& size_, const glm::vec2& position_, uint32_t color_) : StaticObject(size_, position_, color_)
-	{}
+	{
+	}
 
 	static const uint32_t defaultColor = Utils::rgb_to_uint32(255, 255, 255);
+
+	static std::vector<Brick> GenerateBricks(const glm::vec2& brickSize, const glm::vec2& startDraw, const glm::vec2& endDraw)
+	{
+		auto getBrickPosition = [&startDraw, &brickSize](size_t i, size_t j, const glm::vec2& distance)
+		{
+			return startDraw + glm::vec2(i * (brickSize.x + distance.x), j * (brickSize.y + distance.y));
+		};
+
+		size_t bricksX = static_cast<size_t>((endDraw.x - startDraw.x) / brickSize.x);
+		size_t bricksY = static_cast<size_t>((endDraw.y - startDraw.y) / brickSize.y);
+
+		auto lastBrickCoord = getBrickPosition(bricksX - 1, bricksY - 1, { 0, 0 }) + brickSize;
+
+		glm::vec2 distanceBetweenBricks = { endDraw - lastBrickCoord };
+		distanceBetweenBricks.x /= bricksX;
+		distanceBetweenBricks.y /= bricksY;
+
+		std::vector<Brick> bricks;
+		bricks.reserve(bricksX * bricksY);
+
+		for (size_t i = 0; i < bricksX; i++)
+		{
+			for (size_t j = 0; j < bricksY; j++)
+			{
+				glm::vec2 brickPosition = getBrickPosition(i, j, distanceBetweenBricks);
+				bricks.emplace_back(brickSize, brickPosition, Brick::defaultColor);
+			}
+		}
+
+		return bricks;
+	}
 };
--- src/GameObjects/Level.cpp
+++ src/GameObjects/Level.cpp
@@ -8,18 +8,8 @@ std::shared_ptr<Level> Level::Get()
 
 Level::Level()
 {
-	size_t width = 10, height = 5;
-	bricks.reserve(width * height);
-
-	glm::vec2 brickSize{ 80, 50 };
-
-	for (size_t i = 0; i < width; i++)
-	{
-		for (size_t j = 0; j < height; j++)
-		{
-			bricks.emplace_back(brickSize, glm::vec2(2 * wallThinkness + i * (brickSize.x + 10), 2 * wallThinkness + j * (brickSize.y + 10)), Brick::defaultColor);
-		}
-	}
+	glm::vec2 brickSize{ 100, 70 };
+	bricks = Brick::GenerateBricks(brickSize, glm::vec2(wallThinkness, wallThinkness), glm::vec2(SCREEN_WIDTH - wallThinkness, SCREEN_HEIGHT / 2));
 }
 
 void Level::Draw() const
